600cbd56033ecec0ae0710a52a18266aab09f77a
Mayur Bhavsar <wondercrazy15@gmail.com>
2019-01-09 18:30:49 +0530

    commited 09-01-2019 with some new screens

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,6 +5,84 @@ import { Component } from '@angular/core';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  constructor(public statusBar: StatusBar,
+    public router: Router,
+    public mediaCapture: MediaCapture,
+    public camera: Camera,
+    public actionSheetController: ActionSheetController,
+    public loadingController:LoadingController) {
+  //  this.statusBar.styleDefault();
+  this.loadingController.dismiss();
+  }
+  ngOnInit() {
+  }
+  moveToprofile() {
+    this.router.navigateByUrl("/profile");
+  }
+  moveTofilter() {
+    this.router.navigateByUrl("/filter");
 
+  }
+
+  videoRecord() {
+    this.mediaCapture.captureVideo()
+      .then(
+        (data: MediaFile[]) => console.log(data),
+        (err: CaptureError) => console.error(err)
+      );
+
+  }
+
+  videoFromLibrary() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      //  encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.VIDEO
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+      console.log("base64:", base64Image)
+    }, (err) => {
+      // Handle error
+    });
+  }
+
+
+  async videoOptions() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Choose any one',
+      buttons: [{
+        text: 'Video record',
+        // role: 'destructive',
+        // icon: 'trash',
+        handler: () => {
+          this.videoRecord();
+        }
+      }, {
+        text: 'Video from library',
+        //   icon: 'share',
+        handler: () => {
+          this.videoFromLibrary();
+        }
+      }, {
+        text: 'Cancel',
+     //   icon: 'close',
+        role: 'cancel',
+        handler: () => {
+      
+        }
+      }]
+    });
+    await actionSheet.present();
+  }
+
+
+
+ 
 }
